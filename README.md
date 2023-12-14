b40f316b7b805bc52ed865aacccaced9f68cda5c
Jacob Keisling <contact@keisling.me>
2023-12-13 22:48:29 -0600

    Clean up icon assets

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # Jacob Keisling's personal website
 
-## Colophon
+## Architecture
 
-This website is set in Basier Circle and Basier Mono
-typefaces. It's built with Next.js and Tailwind
-and hosted on Vercel.
+This is a [Next.js 14](https://nextjs.org) site, using server components.
+It's currently deployed using Vercel. (Yes, they got me, for now.)
+
+## Local development
+
+### Installation
+
+At the root, install packages:
+
+```bash
+# NVM prefered
+nvm use 20
+npm i
+```
+
+### Usage
+
+Use the standard Next.js commands:
+
+```bash
+# Start server
+npm run dev
+
+# Build for production
+npm run build
+# Serve
+npm run start
+```
--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Logo } from '../Logo'
+import { LogoGithub } from '@carbon/icons-react'
 
 import { Container } from '../Container'
 
@@ -8,18 +9,17 @@ export const Footer = () => {
     <Container>
       <footer className="border-gray flex items-center border-t pt-3 pb-16 text-xs text-gray-400">
         <div className="mr-3 h-6 w-6">
-          <Logo className="h-auto w-auto text-gray-800" />
+          <Logo className="h-6 w-6 text-gray-400" />
         </div>
-        <p>© Jacob Keisling</p>
+        <p>© Jacob Keisling {}</p>
         <div className="ml-auto">
           <a
             className="flex items-center justify-center gap-2"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://github.com/endlessreform/site"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Powered by{' '}
-            <Image src="/vercel.svg" alt="Vercel Logo" width="72" height="16" />
+            <LogoGithub size={20} />
           </a>
         </div>
       </footer>
